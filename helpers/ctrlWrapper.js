ea42ba4532b2581eabb64810f8b5c59f8ec22f1b
VirLg <19virlg@gmail.com>
2023-10-08 22:54:15 +0300

    add

--- helpers/ctrlWrapper.js
+++ helpers/ctrlWrapper.js
@@ -2,7 +2,6 @@ const ctrlWrapper = ctrl => {
   const func = async (req, res, next) => {
     try {
       await ctrl(req, res, next);
-      console.log('ctrl', req);
     } catch (error) {
       console.log('errorCTR', error);
       next(error);
