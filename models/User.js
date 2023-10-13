a27194f4ffbb8df41dce2285395d81ff6caa5220
VirLg <19virlg@gmail.com>
2023-10-13 09:23:37 +0300

    add

---
+++ models/User.js
@@ -0,0 +1,25 @@
+import Joi from 'joi';
+import { Schema, model } from 'mongoose';
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
---
+++ routes/api/auth-router.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import { Router } from 'express';
+
+const authRouter = Router();
+
+export default authRouter;
